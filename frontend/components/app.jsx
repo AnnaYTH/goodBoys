fbf918a6e26b90126e8750fb56d8ed1d4a002995
anna <annayth@gmail.com>
2022-01-02 22:22:11 -0500

    webpack please, but started greeting container

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -9,21 +9,33 @@ import {
 } from 'react-router-dom';
 
 
-import SignUpFormContainer from './session_form/signup_form_container';
-import LogInFormContainer from './session_form/login_form_container';
-import SearchContainer from './search/search_container';
+// import SignUpFormContainer from './session_form/signup_form_container';
+// import LogInFormContainer from './session_form/login_form_container';
+// import SearchContainer from './search/search_container';
 
+
+
+// const App = () => (
+//   <div>
+//     <header>
+//         <h1>Hey Buddy</h1>
+//     </header>
+//     <Switch>
+//       <AuthRoute exact path="/login" component={LogInFormContainer} />
+//       <AuthRoute exact path="/signup" component={SignUpFormContainer} />
+//       <Route exact path="/" component={SearchContainer} />
+//     </Switch>
+//   </div>
+// );
+
+// export default App;
 
 const App = () => (
   <div>
     <header>
         <h1>Hey Buddy</h1>
     </header>
-    <Switch>
-      <AuthRoute exact path="/login" component={LogInFormContainer} />
-      <AuthRoute exact path="/signup" component={SignUpFormContainer} />
-      <Route exact path="/" component={SearchContainer} />
-    </Switch>
+    <Provider store={store}> SOMETHING WILL ROUTE HERE </Provider>
   </div>
 );
 
--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import ReactDOM from "react-dom";
+import configureStore from "./store/store";
+import Root from "./components/root";
 
 document.addEventListener("DOMContentLoaded", () => {
+  // const root = document.getElementById("root");
+  // ReactDOM.render(<h1>Welcome to goodBoys</h1>, root);
+
+  const store = configureStore();
+
+  // // TESTING START
+  // window.getState = store.getState;
+  // window.dispatch = store.dispatch;
+  // // TESTING END
+
   const root = document.getElementById("root");
-  ReactDOM.render(<h1>Welcome to goodBoys</h1>, root);
+  ReactDOM.render(<Root store={store} />, root);
 });
