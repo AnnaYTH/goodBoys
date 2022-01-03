d003b97b0cef04e8c588fb2f62b49e6062d7d173
anna <annayth@gmail.com>
2022-01-02 20:47:29 -0500

    oh webpack is not running, but I need to keep working and I'll figure it out later

---
+++ frontend/components/app.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import {
+  Route,
+  Redirect,
+  Switch,
+  Link,
+  HashRouter
+} from 'react-router-dom';
+
+
+import SignUpFormContainer from './session_form/signup_form_container';
+import LogInFormContainer from './session_form/login_form_container';
+import SearchContainer from './search/search_container';
+
+
+const App = () => (
+  <div>
+    <header>
+        <h1>Hey Buddy</h1>
+    </header>
+    <Switch>
+      <AuthRoute exact path="/login" component={LogInFormContainer} />
+      <AuthRoute exact path="/signup" component={SignUpFormContainer} />
+      <Route exact path="/" component={SearchContainer} />
+    </Switch>
+  </div>
+);
+
+export default App;
