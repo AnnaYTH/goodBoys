ca67dbf82e17ebfad5ce7324d7e2b6a74a52c4f7
anna <annayth@gmail.com>
2022-01-05 18:04:13 -0500

    everything is wonky, but it works, but it is in the valley of uncanny

--- frontend/components/session_form/login_form.jsx
+++ frontend/components/session_form/login_form.jsx
@@ -62,25 +62,49 @@ class LoginForm extends React.Component {
 
     render() {
         return (
-            <div>
-                <button onClick = {this.handleDemoUser}>Demo User</button>
-                <h2>Sign In</h2>
-                <form onSubmit = {this.handleSubmit}>
-                    {this.errorMessages()}
-                    <label> Email:
-                        <input type="text" value = {this.state.email} onChange = {this.update("email")} />
-                    </label>
-                  
-                    <label> Password:
-                        <input type="password" value = {this.state.password} onChange = {this.update("password")} />
-                    </label>
-                   
-                   
-                    <input type="submit" value = "Sign In" />
-                </form>
-                {/* <button onClick={this.handleDemoUser}>
-                    Demo User</button> */}
-                <Link to ="signup">Create Account</Link>
+            <div className='body'>
+                <div className='header'> 
+                    <img src={window.gdby_logo} alt='logo' /> 
+                </div>
+
+                <div className='signup-body'>
+                    <form className='signup-form' onSubmit = {this.handleSubmit}>
+                        <h2>Sign in to GoodBoys</h2>
+
+                        {this.errorMessages()}
+
+                        <div className='email-input input'>
+                            <label> Email
+                                <div className='input-box'>
+                                    <input type="text" value = {this.state.email} onChange = {this.update("email")} />
+                                </div>
+                            </label>
+                        </div>
+                    
+                        <div className='password-input input'>
+                            <label> Password
+                                <div className='input-box'>
+                                    <input type="password" value = {this.state.password} onChange = {this.update("password")} />
+                                </div>
+                            </label>
+                        </div>
+                        
+                        <div>
+                            <input className='signup-buttons' type="submit" value = "Sign In" />
+                        </div>
+
+                        <div>
+                            <button  className='signup-buttons' onClick = {this.handleDemoUser}>Demo User</button>
+                        </div>
+
+                        <div> 
+                            <p className='already-text'>
+                                Not a member? 
+                            </p>       
+                            <Link to ="signup">Sign up</Link>
+                        </div>
+                    </form>
+                </div>
             </div>
         )
     }
