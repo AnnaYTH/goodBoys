dc15eb0d8268b91cb1f8703207365aa91e06bc1c
anna <annayth@gmail.com>
2022-01-11 09:25:42 -0500

    working on css for individual relationship page

--- frontend/components/relationships/relationship_ship.jsx
+++ frontend/components/relationships/relationship_ship.jsx
@@ -11,10 +11,18 @@ class RelationshipShip extends React.Component {
     }
 
     componentDidMount() {
+        // debugger;
         this.props.getRelationships(); 
+        // this.props.getRelationship(this.props.match.params.id); 
+        // this is unnecessary because I just need to populate once with all relationships
     }
 
     render() {
+        const ship = this.props.relationship
+        // [this.props.match.params.id]; 
+        if(!ship) {
+            return null
+        }
         return (
             <div className='one-ship-body'>
      
@@ -27,11 +35,14 @@ class RelationshipShip extends React.Component {
                         <p className='ship_profile_image'> This is an image </p>
                         <button className='button'> Rate this Relationship </button>
                     </div>
-
+                    <div>
+                        
+                    </div>
                     <div className='ship_col2'>
-                        <h1>Title</h1>
-                        <h2>Pronounciation</h2>
-                        <h3>Definition</h3>
+
+                        <h1> {ship.relationship_name} </h1>
+                        <h2> {ship.pronounciation} </h2>
+                        <h3>{ship.relationship_definition}</h3>
                         <h4>Reviews</h4>
                     </div>
                 </div>
