ebd194a709b48874d57df82bddbf52d44467e73c
anna <annayth@gmail.com>
2022-01-12 11:11:12 -0500

    changed routes to protected and auth

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -13,6 +13,7 @@ import LoginFormContainer from './session_form/login_form_container';
 import Splash from './splash';
 import RelationshipContainer from './relationships/relationship_container'; 
 import RelationshipShipContainer from './relationships/relationship_ship_container'; 
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
 
 
 const App = () => (
@@ -24,11 +25,11 @@ const App = () => (
     </header> */}
     <p> </p>
     <Switch>
-      <Route path="/login" component={LoginFormContainer} />
-      <Route path="/signup" component={SignupFormContainer} />
-      <Route exact path="/relationships" component={RelationshipContainer} />
-      <Route exact path="/relationships/:id" component={RelationshipShipContainer} />
-      <Route exact path= "/" component={Splash}/>
+      <AuthRoute path="/login" component={LoginFormContainer} />
+      <AuthRoute path="/signup" component={SignupFormContainer} />
+      <ProtectedRoute exact path="/relationships" component={RelationshipContainer} />
+      <ProtectedRoute exact path="/relationships/:id" component={RelationshipShipContainer} />
+      <AuthRoute exact path= "/" component={Splash}/>
     </Switch>
     
   </div>
