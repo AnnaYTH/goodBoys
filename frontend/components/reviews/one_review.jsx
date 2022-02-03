8752be93883913a0378e22c8ba9acea8ee8aa5d6
anna <annayth@gmail.com>
2022-02-02 22:25:43 -0500

    render singular review show page using return null

--- frontend/components/reviews/one_review.jsx
+++ frontend/components/reviews/one_review.jsx
@@ -9,6 +9,7 @@ const mSTP = (state, ownProps) => {
     return ({
         reviews: state.entities.reviews,
         currentUser: state.entities.users[state.session.id],
+        currentReview: state.entities.reviews[ownProps.match.params.id],
     })
 }
 
@@ -18,15 +19,45 @@ const mDTP = dispatch => ({
 }); 
 
 class OneReview extends React.Component {
+    constructor(props) {
+        super(props); 
+    }
+
+    componentDidMount() {
+        this.props.getReviews(); 
+        this.props.getReview(this.props.match.params.id); 
+    }
+
+    // componentDidUpdate(prevProps) {
+    //     if(prevProps.reviews !== this.props.reviews) {
+    //         this.props.getReviews(); 
+    //     }
+    // }  
 
     render() {
+        const currentReview = this.props.currentReview; 
+
+        if (!currentReview) {
+            return null; 
+        }
+
+        // debugger; 
         return (
             <div>
                 <Header />
                 <div>
-                    This is a singular Review 
+                    This is a singular Review -  # {this.props.match.params.id}
                 </div>
-
+                <div>
+                    <h1>{currentReview.id}</h1>
+                    <p>relationship ID: {currentReview.relationship_id}</p>
+                    <p>user ID: {currentReview.user_id}</p>
+                    <p>name: {currentReview.name}</p>
+                    <p>rating: {currentReview.rating}</p>
+                    <p>review: {currentReview.review} </p>
+                    <p>date reviewed: {currentReview.created_at.split("T")[0]}</p>
+                </div>
+                <button> Edit </button>
                 <Footer />
             </div>
         )
