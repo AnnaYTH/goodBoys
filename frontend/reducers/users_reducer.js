e08a503b20dff977d3d805ab36c9ffbcc296445b
anna <annayth@gmail.com>
2022-02-11 21:23:10 -0500

    about to refactor actionsO

--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -10,7 +10,7 @@ const usersReducer = (state = {}, action) => {
     case RECEIVE_CURRENT_USER:
       return Object.assign({}, state, { [action.currentUser.id]: action.currentUser });
     case ADD_BUDDY: 
-      debugger; 
+      // debugger; 
     case REMOVE_BUDDY: 
       const nextState = Object.assign({}, state)
       delete nextState[action.user.id]
