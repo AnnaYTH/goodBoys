9d90703154ba9f2c6ea78752e99d9cf1a467a6e6
anna <annayth@gmail.com>
2022-02-14 21:41:07 -0500

    middle of seeding filler reviews from a tail of two cities, haha get it? tail bc dogs...

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,21 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(name: 'Anna', email: 'demo@demo.com', password: 'password');
-User.create(name: 'Buddy', email: 'buddy@gmail.com', password: 'hunter12');
+User.create(name: 'Anna TroHer', email: 'demo@demo.com', password: 'password');
+User.create(name: 'Buddy Demogod', email: 'buddy@gmail.com', password: 'hunter12');
 User.create(name: 'Tashi Sangpo', email: 'tashi@gmail.com', password: 'password'); 
 User.create(name: 'Marco Countryman', email: 'marco@gmail.com', password: 'password'); 
 User.create(name: 'Christian Lee', email: 'clee@gmail.com', password: 'password'); 
 User.create(name: 'Andrew Vitovich', email: 'avito@gmail.com', password: 'password'); 
-User.create(name: 'Andrew Vitovich', email: 'avito@gmail.com', password: 'password'); 
+User.create(name: 'Jay Gatsby', email: 'gatsby@gmail.com', password: 'password'); 
+User.create(name: 'Holden Caulfield', email: 'caulfield@gmail.com', password: 'password'); 
+User.create(name: 'Sherlock Holmes', email: 'holmes@demo.com', password: 'password');
+User.create(name: 'Atticus Finch', email: 'finch@demo.com', password: 'password');
+User.create(name: 'The Invisible Man', email: 'invisible@demo.com', password: 'password');
+User.create(name: 'Yossarian', email: 'yoss@demo.com', password: 'password');
+User.create(name: 'Scout Finch', email: 'sfinch@demo.com', password: 'password');
+User.create(name: 'James Bond', email: 'bond@demo.com', password: 'password');
+User.create(name: 'Mary Katherine Blackwood', email: 'blackwood@demo.com', password: 'password');
 
 Follow.create(user_id: 2, follow_id: 1); 
 
@@ -247,11 +255,73 @@ Review.create(
     rating: 5, 
     body: 'He\'s a wizard'
 )
-
 Review.create(
     user_id: 2, 
     relationship_id: 1, 
     name: 'Harry Potty', 
     rating: 5, 
     body: 'He\'s a turlet'
+)
+Review.create(
+    user_id: 3, 
+    relationship_id: 2, 
+    name: 'Judith Boss', 
+    rating: 5, 
+    body: 'It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way—in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only'
+)
+Review.create(
+    user_id: 4, 
+    relationship_id: 3, 
+    name: 'Darkness', 
+    rating: 5, 
+    body: 'There were a king with a large jaw and a queen with a plain face, on the throne of England; there were a king with a large jaw and a queen with a fair face, on the throne of France. In both countries it was clearer than crystal to the lords of the State preserves of loaves and fishes, that things in general were settled for ever.'
+)
+Review.create(
+    user_id: 5, 
+    relationship_id: 4, 
+    name: 'Mrs. Southcott', 
+    rating: 5, 
+    body: 'It was the year of Our Lord one thousand seven hundred and seventy-five. Spiritual revelations were conceded to England at that favoured period, as at this. Mrs. Southcott had recently attained her five-and-twentieth blessed birthday, of whom a prophetic private in the Life Guards had heralded the sublime appearance by announcing that arrangements were made for the swallowing up of London and Westminster. Even the Cock-lane ghost had been laid only a round dozen of years, after rapping out its messages, as the spirits of this very year last past (supernaturally deficient in originality) rapped out theirs. Mere messages in the earthly order of events had lately come to the English Crown and People, from a congress of British subjects in America: which, strange to relate, have proved more important to the human race than any communications yet received through any of the chickens of the Cock-lane brood.'
+)
+Review.create(
+    user_id: 6, 
+    relationship_id: 4, 
+    name: 'Woodman', 
+    rating: 5, 
+    body: 'France, less favoured on the whole as to matters spiritual than her sister of the shield and trident, rolled with exceeding smoothness down hill, making paper money and spending it. Under the guidance of her Christian pastors, she entertained herself, besides, with such humane achievements as sentencing a youth to have his hands cut off, his tongue torn out with pincers, and his body burned alive, because he had not kneeled down in the rain to do honour to a dirty procession of monks which passed within his view, at a distance of some fifty or sixty yards. It is likely enough that, rooted in the woods of France and Norway, there were growing trees, when that sufferer was put to death, already marked by the Woodman, Fate, to come down and be sawn into boards, to make a certain movable framework with a sack and a knife in it, terrible in history. It is likely enough that in the rough outhouses of some tillers of the heavy lands adjacent to Paris, there were sheltered from the weather that very day, rude carts, bespattered with rustic mire, snuffed about by pigs, and roosted in by poultry, which the Farmer, Death, had already set apart to be his tumbrils of the Revolution. But that Woodman and that Farmer, though they work unceasingly, work silently, and no one heard them as they went about with muffled tread: the rather, forasmuch as to entertain any suspicion that they were awake, was to be atheistical and traitorous.'
+)
+Review.create(
+    user_id: 7, 
+    relationship_id: 5, 
+    name: '', 
+    rating: 5, 
+    body: ''
+)
+Review.create(
+    user_id: 8, 
+    relationship_id: 6, 
+    name: 'Turnham Green', 
+    rating: 5, 
+    body: ''
+)
+Review.create(
+    user_id: 9, 
+    relationship_id: 7, 
+    name: '', 
+    rating: 5, 
+    body: ''
+)
+Review.create(
+    user_id: 10, 
+    relationship_id: 8, 
+    name: '', 
+    rating: 5, 
+    body: ''
+)
+Review.create(
+    user_id: 11, 
+    relationship_id: 9, 
+    name: '', 
+    rating: 5, 
+    body: ''
 )
